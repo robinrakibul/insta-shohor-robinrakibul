b35869a1604c2921167fbc7260cb02360ace2b28
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-10 21:00:48 +0600

    solved like button

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    //1. Issue fixed the like button was not working. 
+    likedPostsId.push(id); 
     showPosts(posts);
 };
 
